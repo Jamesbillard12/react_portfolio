79138a084cc33c1ace83fac140c2e4ee7eb3cb75
Jamesbillard12 <jamesbillard12@hotmail.com>
2018-06-09 10:27:26 -0700

    removed id and date because it is not needed

--- action/scrollTop-actions.js
+++ action/scrollTop-actions.js
@@ -1,8 +1,6 @@
 import uuid from 'uuid/v1'
 
 export const scrollTopCreate = scroll => {
-	scroll.id = uuid()
-	scroll.timestamp = new Date()
 	return {
 		type: 'SCROLLTOP_CREATE',
 		payload: scroll
--- reducer/scrolltop.js
+++ reducer/scrolltop.js
@@ -1,16 +1,4 @@
-// let validateScroll = scroll => {
-// 	console.log(scroll)
-// 	if (!scroll.id || !scroll.scrollTop || !scroll.timestamp) {
-// 		throw new Error(
-// 			'VALIDATION_ERROR: scroll must include id, scrollTop, and timestamp'
-// 		)
-// 	}
-// }
-
-export default (
-	state = { id: 1, scrollTop: 0, timestamp: new Date() },
-	action
-) => {
+export default (state = { scrollTop: 0 }, action) => {
 	let { type, payload } = action
 
 	switch (type) {
